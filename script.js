e6d0a8009bbab5ba1117394d52223b67357dbe98
RafM <rkjmosiolek@gmail.com>
2017-03-31 13:40:01 +0200

    darken/lighten button working

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ for (var i = 1; i <= 5; i++) {
 
 }
 /* Wiring up the Darken/Lighten button */
-
-
-
+btn.onclick = function() {
+    if (btn.getAttribute("class") === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+}
